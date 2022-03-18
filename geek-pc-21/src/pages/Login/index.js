febfb7c1d1433f2e7fd22120157d03d08d7798d9
liujingqi <765712426@qq.com>
2022-03-18 16:15:27 +0800

    项目绝对路径的使用和sass的配置

--- geek-pc-21/src/pages/Login/index.js
+++ geek-pc-21/src/pages/Login/index.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react'
 import { Card } from 'antd'
-import './index.css'
+import './index.scss'
 //引入图片方法
-// import logo from '../../assets/logo.png'
+import logo from 'assets/logo.jpg'
 export default class Login extends Component {
     render() {
         return (
             <div className="login">
                 <Card className="login-container">
                     {/* 引入图片 */}
-                    {/* <img src={logo} alt="" /> */}
+                    <img src={logo} alt="" />
                     <h1>我的校园</h1>
                     <p>Card content</p>
                     <p>Card content</p>
