fb5b0a7d31d7a7edeb6a426e6f36792d2d43e760
liujingqi <765712426@qq.com>
2022-03-18 09:09:26 +0800

    antd 组件库的使用

--- geek-pc-21/src/App.js
+++ geek-pc-21/src/App.js
@@ -1,5 +1,5 @@
 
-import { BrowserRouter as  Router, Route, Link, Switch} from 'react-router-dom'
+import { BrowserRouter as  Router, Route, Switch} from 'react-router-dom'
 import Home from './pages/Layout'
 import Login from './pages/Login'
 function App() {
--- geek-pc-21/src/pages/Login/index.js
+++ geek-pc-21/src/pages/Login/index.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react'
-
+import { Button } from 'antd'
 export default class Login extends Component {
     render() {
-        return <div>登录布局组件</div>
+        return (
+            <div>
+                登录布局组件 <Button type="primary">我是一个按钮</Button>
+            </div>            
+        )
+
     }
 }
