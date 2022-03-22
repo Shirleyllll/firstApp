6fb736262d48713e934eaf886298bd0b34186f3e
liujingqi <765712426@qq.com>
2022-03-22 15:18:03 +0800

    css modules 的基本使用

--- geek-pc-21/src/pages/Layout/index.js
+++ geek-pc-21/src/pages/Layout/index.js
@@ -1,7 +1,14 @@
 import React, { Component } from 'react'
-
+//css modules会自动对样式文件中的所有的选择器命名
+import styles from './index.module.css'
 export default class Layout extends Component {
     render() {
-        return <div>首页布局组件</div>
+        return (
+        <div>
+            <div className={styles.aa}>首页布局组件</div>
+            <div className={styles.bb}>bb</div>
+
+        </div>
+        )
     }
 }
